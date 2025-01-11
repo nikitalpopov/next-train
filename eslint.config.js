a6fd8cc661dd3627d834eb864d465c31b895ef41
Nikita Popov <mailto.nikitapopov@gmail.com>
2025-01-11 18:33:55 +0100

    chore: add linting

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import { readFileSync } from 'node:fs'
+
+const gitignore = readFileSync('.gitignore', 'utf8').split('\n').filter(Boolean)
+
+export default [
+  {
+    ignores: gitignore,
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
+]
---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  proseWrap: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+}
