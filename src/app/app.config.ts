ee70663875c3757f4770cc4dda115c30693f811f
Nikita Popov <mailto.nikitapopov@gmail.com>
2025-03-09 21:55:46 +0100

    chore: setup Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,24 @@
 import { provideHttpClient } from '@angular/common/http'
 import type { ApplicationConfig } from '@angular/core'
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app'
+import { getAuth, provideAuth } from '@angular/fire/auth'
 import { provideRouter } from '@angular/router'
-
 import { routes } from './app.routes'
 
+const firebaseConfig = {
+  apiKey: import.meta.env.NG_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.NG_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.NG_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.NG_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.NG_APP_FIREBASE_APP_ID,
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+  ],
 }
--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,12 @@ declare interface Env {
   // Example: NGX_VERSION: string;
   // [key: string]: unknown
   NG_APP_MAPBOX_TOKEN: string
+  NG_APP_FIREBASE_API_KEY: string
+  NG_APP_FIREBASE_AUTH_DOMAIN: string
+  NG_APP_FIREBASE_PROJECT_ID: string
+  NG_APP_FIREBASE_STORAGE_BUCKET: string
+  NG_APP_FIREBASE_MESSAGING_SENDER_ID: string
+  NG_APP_FIREBASE_APP_ID: string
 }
 
 // Choose how to access the environment variables.
